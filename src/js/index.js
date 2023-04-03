18b86388b1f9aa1839406533df5f4ca6cf1b83f0
LuisPontesLS <leospontes@hotmail.com>
2023-04-03 18:34:55 -0300

    ajuste video

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
-/*
-OBJETIVO 1 - Quando clicarem no botão ver trailer, devemos abrir o modal com o video trailer
-*/
+const botaoTrailer = document.querySelector(".botao-trailer");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = "https://www.youtube.com/embed/Cb4WV4aXBpk";
+
+// - Passo 2: identificar quando o usuário clicar neste botão 
+botaoTrailer.addEventListener("click", () => {
+    // Passo 4: abrir a modal na tela.
+    modal.classList.add("aberto");
+});
+
+// Obj1 - Passo 3: pegar o elemento da modal no JS;
+// Ob2 Passo 1: pegar o elemento de fechar modal;
+const botaoFecharModal = document.querySelector(".fechar-modal");
+
+//Passo 2: identificar quando o 'x' do modal for clicado;
+botaoFecharModal.addEventListener("click", () => {
+    //passo 3 - fechar modal
+    modal.classList.remove("aberto");
+    video.setAttribute("src", "");
+});
+
