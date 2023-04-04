bef775d71d9bb1ee2f750e716be5d8c3221f9b6d
LuisPontesLS <leospontes@hotmail.com>
2023-04-04 16:53:12 -0300

    finalizando JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,15 @@
+const botaoFecharModal = document.querySelector(".fechar-modal");
 const botaoTrailer = document.querySelector(".botao-trailer");
 const video = document.getElementById("video");
 const modal = document.querySelector(".modal");
-const linkDoVideo = "https://www.youtube.com/embed/Cb4WV4aXBpk";
+const linkDoVideo = video.src;
 
-// - Passo 2: identificar quando o usuário clicar neste botão 
 botaoTrailer.addEventListener("click", () => {
-    // Passo 4: abrir a modal na tela.
-    modal.classList.add("aberto");
+    modal.classList.add("aberto");/*toogle no lugar de add 1:38:00*/ 
+    video.setAttribute("src", linkDoVideo)
 });
 
-// Obj1 - Passo 3: pegar o elemento da modal no JS;
-// Ob2 Passo 1: pegar o elemento de fechar modal;
-const botaoFecharModal = document.querySelector(".fechar-modal");
-
-//Passo 2: identificar quando o 'x' do modal for clicado;
 botaoFecharModal.addEventListener("click", () => {
-    //passo 3 - fechar modal
     modal.classList.remove("aberto");
     video.setAttribute("src", "");
 });
